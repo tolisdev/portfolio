42e3455fd8c7e1fccae952f75c732c2d252d6084
Developer <developer@example.com>
2026-08-11 18:07:32 +0300

    Content update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,18 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://alexvance.dev',
   adapter: node({
     mode: 'standalone',
   }),
-  integrations: [react(), keystatic()],
+  integrations: [react(), keystatic(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
